b5d369f3d95781049cca5dcc6e3b1f82e0aabcf1
Võ Hữu Tài <90029952+vohuutai23@users.noreply.github.com>
2025-02-23 16:52:47 +0700

    feat: setup model and user router

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 import { formatDate, schemaOptions } from "../configurations/schemaConfig.js";
-// import Message from "./messageModel.js";
-// import ConversationMember from "./conversationMemberModel.js";
-// import Conversation from "./conversationModel.js";
-// import Comment from "./commentModel.js";
-// import MessageReaction from "./messageReactionModel.js";
-// import PostReaction from "./postReactionModel.js";
-// import Post from "./postModel.js";
-// import Notification from "./notificationModel.js";
-// import CommentReaction from "./commentReactionModel.js";
-// import Friendship from "./friendshipModel.js";
-// import Story from "./storyModel.js";
-// import { deleteFileByUrl } from "../services/apiService.js";
+import Message from "./messageModel.js";
+import ConversationMember from "./conversationMemberModel.js";
+import Conversation from "./conversationModel.js";
+import Comment from "./commentModel.js";
+import MessageReaction from "./messageReactionModel.js";
+import PostReaction from "./postReactionModel.js";
+import Post from "./postModel.js";
+import Notification from "./notificationModel.js";
+import CommentReaction from "./commentReactionModel.js";
+import Friendship from "./friendshipModel.js";
+import Story from "./storyModel.js";
+import { deleteFileByUrl } from "../services/apiService.js";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -82,52 +82,52 @@ const UserSchema = new mongoose.Schema(
   schemaOptions
 );
 
-// UserSchema.pre(
-//   "deleteOne",
-//   { document: true, query: false },
-//   async function (next) {
-//     try {
-//       await deleteFileByUrl(this.avatarUrl);
-//       const conversations = await Conversation.find({ owner: this._id });
-//       for (const conversation of conversations) {
-//         await conversation.deleteOne();
-//       }
-//       const messages = await Message.find({ user: this._id });
-//       for (const message of messages) {
-//         await message.deleteOne();
-//       }
-//       const comments = await Comment.find({ user: this._id });
-//       for (const comment of comments) {
-//         await comment.deleteOne();
-//       }
-//       const posts = await Post.find({ user: this._id });
-//       for (const post of posts) {
-//         await post.deleteOne();
-//       }
-//       const stories = await Story.find({ user: this._id });
-//       for (const story of stories) {
-//         await story.deleteOne();
-//       }
-//       const friendships = await Friendship.find({
-//         $or: [{ sender: this._id }, { receiver: this._id }],
-//       });
-//       for (const friendship of friendships) {
-//         await friendship.deleteOne();
-//       }
-//       await Notification.deleteMany({
-//         "data.user._id": this._id,
-//       });
-//       await Notification.deleteMany({ user: this._id });
-//       await ConversationMember.deleteMany({ user: this._id });
-//       await MessageReaction.deleteMany({ user: this._id });
-//       await PostReaction.deleteMany({ user: this._id });
-//       await CommentReaction.deleteMany({ user: this._id });
-//       next();
-//     } catch (error) {
-//       next(error);
-//     }
-//   }
-// );
+UserSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function (next) {
+    try {
+      await deleteFileByUrl(this.avatarUrl);
+      const conversations = await Conversation.find({ owner: this._id });
+      for (const conversation of conversations) {
+        await conversation.deleteOne();
+      }
+      const messages = await Message.find({ user: this._id });
+      for (const message of messages) {
+        await message.deleteOne();
+      }
+      const comments = await Comment.find({ user: this._id });
+      for (const comment of comments) {
+        await comment.deleteOne();
+      }
+      const posts = await Post.find({ user: this._id });
+      for (const post of posts) {
+        await post.deleteOne();
+      }
+      const stories = await Story.find({ user: this._id });
+      for (const story of stories) {
+        await story.deleteOne();
+      }
+      const friendships = await Friendship.find({
+        $or: [{ sender: this._id }, { receiver: this._id }],
+      });
+      for (const friendship of friendships) {
+        await friendship.deleteOne();
+      }
+      await Notification.deleteMany({
+        "data.user._id": this._id,
+      });
+      await Notification.deleteMany({ user: this._id });
+      await ConversationMember.deleteMany({ user: this._id });
+      await MessageReaction.deleteMany({ user: this._id });
+      await PostReaction.deleteMany({ user: this._id });
+      await CommentReaction.deleteMany({ user: this._id });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 const User = mongoose.model("User", UserSchema);
 export default User;
